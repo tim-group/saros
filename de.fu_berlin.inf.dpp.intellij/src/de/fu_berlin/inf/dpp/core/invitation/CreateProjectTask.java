a59004f51ef71c8f1921e54b48bed5cab70b536a
Holger Schmeisky <google@schmeisky.com>
2014-07-23 08:26:05 +0200

    [REFACTOR] cleaned up test folders
    Change-Id: Id47952bdfac6a5d3b1887cf8e0ba8ce4db6a20d9

--- de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/core/invitation/CreateProjectTask.java
+++ de.fu_berlin.inf.dpp.intellij/src/de/fu_berlin/inf/dpp/core/invitation/CreateProjectTask.java
@@ -1,11 +1,15 @@
 package de.fu_berlin.inf.dpp.core.invitation;
 
+import com.intellij.openapi.application.ApplicationInfo;
+import com.intellij.openapi.project.Project;
+import de.fu_berlin.inf.dpp.core.context.SarosPluginContext;
 import de.fu_berlin.inf.dpp.core.monitor.IProgressMonitor;
 import de.fu_berlin.inf.dpp.core.monitor.NullProgressMonitor;
 import de.fu_berlin.inf.dpp.core.workspace.IWorkspace;
 import de.fu_berlin.inf.dpp.core.workspace.IWorkspaceRunnable;
 import de.fu_berlin.inf.dpp.filesystem.IProject;
 import org.apache.log4j.Logger;
+import org.picocontainer.annotations.Inject;
 
 import java.io.IOException;
 
@@ -13,6 +17,8 @@ import java.io.IOException;
 //FIXME: Adapt more to IntelliJ (see FIXME below)
 public class CreateProjectTask implements IWorkspaceRunnable {
     private static final Logger LOG = Logger.getLogger(CreateProjectTask.class);
+    public static final String INTELLI_J_IDEA = "IntelliJ IDEA";
+    public static final String PHP_STORM = "PhpStorm";
     private final String name;
     private final IProject base;
     private final IProgressMonitor monitor;
@@ -21,6 +27,9 @@ public class CreateProjectTask implements IWorkspaceRunnable {
 
     //TODO: Check if this can be replaced by a static call
     private IWorkspace workspace;
+
+    @Inject
+    private Project intellijProject;
 
     /**
      * Creates a create project task that can be executed by
@@ -37,6 +46,7 @@ public class CreateProjectTask implements IWorkspaceRunnable {
     public CreateProjectTask(String name, IProject base,
                              IProgressMonitor monitor, IWorkspace workspace) {
 
+        SarosPluginContext.initComponent(this);
         this.name = name;
         this.base = base;
         this.monitor = monitor;
@@ -68,6 +78,13 @@ public class CreateProjectTask implements IWorkspaceRunnable {
 
         //FIXME: right now creating a directory for project only, later IntelliJ
         // native implementation should be used to create project structure
+        String IDEAVersion = ApplicationInfo.getInstance().getVersionName();
+        if (IDEAVersion.equals(INTELLI_J_IDEA)) {
+            //ModuleManager.getInstance(intellijProject).newModule(proj);
+        } else if (IDEAVersion.equals(PHP_STORM)) {
+
+        }
+
         if (!project.getFullPath().toFile().mkdirs()) {
             LOG.error("Could not create project: " + project.getName());
             throw new IOException("Could not create project");
